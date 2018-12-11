9652dd7690f70e6958f9bcf7297a1b96badbb70c
Melih <mredzhebov@roc-dev.com>
2018-12-11 12:41:26 +0100

    Initial commit

---
+++ JavaScriptOp2.js
@@ -0,0 +1,4 @@
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("aap1").src = "images/animal1.jpg";
+document.getElementById("aap2").src = "images/aap2.jpeg";
