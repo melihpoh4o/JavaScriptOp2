456c86c920a36558b91df32d53ef333c3a02fe3e
Melih <mredzhebov@roc-dev.com>
2018-12-11 13:42:23 +0100

    hoi

---
+++ JavaScriptOp2-2.js
@@ -0,0 +1,4 @@
+document.getElementById("blokje1").style.backgroundColor = "lightblue";
+document.getElementById("blokje2").style.backgroundColor = "green";
+document.getElementById("blokje3").style.backgroundColor = "yellow";
+document.getElementById("blokje4").style.backgroundColor = "red";
--- JavaScriptOp2.js
+++ JavaScriptOp2.js
@@ -2,3 +2,5 @@ document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
 document.getElementById("subkop").innerText = "Mijn geweldige subkop";
 document.getElementById("aap1").src = "images/animal1.jpg";
 document.getElementById("aap2").src = "images/aap2.jpeg";
+
+
